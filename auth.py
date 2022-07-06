694aba465bcc3eacd0d5c8af3000d34bdfe6a0d4
Masachusets <apofeoz@tut.by>
2022-07-06 22:43:27 +0300

    add search applicants

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@
 import vk_api
 from pprint import pprint
 from VKtoken import access_token
+from datetime import datetime
 
 
 session = vk_api.VkApi(token=access_token)
@@ -15,17 +16,18 @@ def processing_photos(user_id):
                               count=200,
                               photo_sizes=0)
     best_three_photos = sorted(photos['items'], key=lambda x: x['likes']['count'])[-3:]
-    pprint()
+    return [photo['sizes'][-1]['url'] for photo in best_three_photos]
 
 def processing_applicant(user_id):
     user = vk.users.get(user_id=[user_id], fields='bdate,city,country,sex')[0]
-    photos = user['id']
-    # return f"{user['first_name']} {user['last_name']}\n"
+    photos = processing_photos(user['id'])
+    user_url = 'https://vk.com/' + str(user_id)
+    return f"{user['first_name']} {user['last_name']} {user_url} {photos}"
 
 
 def search_applicants(age, user_sex, city_id):
     offset = 0
-    while offset < 30:
+    while offset < 15:
         offset += 1
         applicants = vk.users.search(city=city_id,
                                      sex=user_sex % 2 + 1,
@@ -34,16 +36,15 @@ def search_applicants(age, user_sex, city_id):
                                      has_photo=1,
                                      offset=offset,
                                      count=1)
-
-        yield applicants['items'][0]
+        if applicants['items'][0]['can_access_closed']:
+            yield processing_applicant(applicants['items'][0]['id'])
 
 
 if __name__ == '__main__':
-    # user = vk.users.get(user_id=['angelinuska'], fields='bdate,city,country,sex')[0]
-
-    # processing_applicant('loggvi')
-    processing_photos(151171819)
-    # user_city = user['city']['id']
-    # user_sex = user['sex']
-    # for i in search_applicants(20, user_sex, user_city):
-    #     pprint(i)
+    user = vk.users.get(user_id=['loggvi'], fields='bdate,city,country,sex')[0]
+    user_city = user['city']['id']
+    user_sex = user['sex']
+    user_age_indays = datetime.date(datetime.now()) - datetime.date(datetime.strptime(user['bdate'], '%d.%m.%Y'))
+    user_age = user_age_indays.days // 365
+    for i in search_applicants(user_age, user_sex, user_city):
+        pprint(i)
